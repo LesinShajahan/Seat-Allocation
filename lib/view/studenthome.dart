9cc5dcc26c76f60113d1ddf34a99291f5e36a966
LesinShajahan <lesinshajahan123@gmail.com>
2024-01-09 20:08:13 +0530

    add studenthome.dart

--- lib/view/studenthome.dart
+++ lib/view/studenthome.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class StudentHome extends StatelessWidget {
-  const StudentHome({super.key});
+  const StudentHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 19, 57, 85),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            // Add your menu icon onPressed logic here
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Add your notification icon onPressed logic here
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              // Add user details here, e.g., user profile image
+              backgroundImage:
+                  NetworkImage('https://example.com/user_profile_image.jpg'),
+            ),
+          ),
+        ],
+      ),
+      body: const Placeholder(),
+    );
   }
 }
